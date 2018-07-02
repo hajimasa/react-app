2372104878bda796aba6c5267c0a1e4c91947cf1
hajime <hajimasa2012@gmail.com>
2018-07-02 14:31:59 +0900

    component test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onClick={() => {console.log("I'm clicked")}} />
+//       </div>
+//     )
+//   }
+// }
+
+const App = () => {
     return (
       <div>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onClick={() => {console.log("I'm clicked")}} />
+        <Cat />
+        <Cat />
+        <Cat />
+        <Cat />
       </div>
     )
-  }
 }
 
+
+const Cat = () => {
+  return <div>Meow</div>;
+}
 export default App;
